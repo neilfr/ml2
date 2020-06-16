16b9933402a2437dea14d34e9242bc312b48e52b
n.froggatt <n.froggatt@vehikl.com>
2020-06-16 17:06:08 -0400

    move ingredients tests from foodcontrollertests to ingredientscontrollertests

--- tests/Feature/FoodControllerTest.php
+++ tests/Feature/FoodControllerTest.php
@@ -90,52 +90,6 @@ class FoodControllerTest extends TestCase
     }
 
     /** @test */
-    public function ingredients_have_many_parent_foods()
-    {
-        $user = factory(User::class)->create();
-        $this->actingAs($user);
-
-        $parentfoods = factory(Food::class, 2)->create();
-
-        $ingredient = factory(Food::class)->create([
-            'description' => 'ingredient',
-        ]);
-
-        foreach ($parentfoods as $food) {
-            $ingredient->parentfoods()->attach($food->id, ['quantity' => 555]);
-        }
-
-        foreach ($parentfoods as $food) {
-            $this->assertEquals($food->description, $ingredient->parentfoods()->find($food->id)->description);
-        }
-    }
-
-    /** @test */
-    public function ingredients_have_quantities()
-    {
-        $user = factory(User::class)->create();
-        $this->actingAs($user);
-
-        $ingredient = factory(Food::class)->create([
-            'description' => 'ingredient',
-        ]);
-
-        $parentfood = factory(Food::class)->create([
-            'description' => 'parentfood',
-        ]);
-
-        $parentfood->ingredients()->attach($ingredient->id, ['quantity' => 555]);
-
-        // dd($parentfood->ingredients()->first()->pivot->quantity);
-
-        $this->assertDatabaseHas('ingredients', [
-            'parent_food_id' => $parentfood->id,
-            'ingredient_id' => $ingredient->id,
-            'quantity' => 555,
-        ]);
-    }
-
-    /** @test */
     public function it_can_return_user_owned_foods()
     {
         $user = factory(User::class)->create();
--- tests/Feature/IngredientControllerTest.php
+++ tests/Feature/IngredientControllerTest.php
@@ -2,16 +2,59 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Food;
+use App\User;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class IngredientControllerTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
-    public function fffff()
+    public function ingredients_have_many_parent_foods()
     {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $parentfoods = factory(Food::class, 2)->create();
+
+        $ingredient = factory(Food::class)->create([
+            'description' => 'ingredient',
+        ]);
+
+        foreach ($parentfoods as $food) {
+            $ingredient->parentfoods()->attach($food->id, ['quantity' => 555]);
+        }
+
+        foreach ($parentfoods as $food) {
+            $this->assertEquals($food->description, $ingredient->parentfoods()->find($food->id)->description);
+        }
+    }
+
+    /** @test */
+    public function ingredients_have_quantities()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $ingredient = factory(Food::class)->create([
+            'description' => 'ingredient',
+        ]);
+
+        $parentfood = factory(Food::class)->create([
+            'description' => 'parentfood',
+        ]);
+
+        $parentfood->ingredients()->attach($ingredient->id, ['quantity' => 555]);
+
+        // dd($parentfood->ingredients()->first()->pivot->quantity);
+
+        $this->assertDatabaseHas('ingredients', [
+            'parent_food_id' => $parentfood->id,
+            'ingredient_id' => $ingredient->id,
+            'quantity' => 555,
+        ]);
     }
 }
