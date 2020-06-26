a31ca2cb6732e6206e34d4fd885794a631950919
Neil Froggatt <neilfr@live.com>
2020-06-26 15:58:00 -0400

    Seeders (#19)
    * user, foodsource, foodgroup seeders
    
    * add health canada food seeder
    
    Co-authored-by: n.froggatt <n.froggatt@vehikl.com>

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            UserSeeder::class,
+            FoodsourceSeeder::class,
+            FoodgroupSeeder::class,
+            FoodSeeder::class,
+        ]);
         // $this->call(UserSeeder::class);
     }
 }
---
+++ database/seeds/FoodgroupSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FoodgroupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::insert(" INSERT INTO `foodgroups` (`id`, `description`, `created_at`) VALUES
+            (1,'Dairy and Egg Products', now()),
+            (2,'Spices and Herbs', now()),
+            (3,'Babyfoods', now()),
+            (4,'Fats and Oils', now()),
+            (5,'Poultry Products', now()),
+            (6,'Soups, Sauces and Gravies', now()),
+            (7,'Sausages and Luncheon meats', now()),
+            (8,'Breakfast cereals', now()),
+            (9,'Fruits and fruit juices', now()),
+            (10,'Pork Products', now()),
+            (11,'Vegetables and Vegetable Products', now()),
+            (12,'Nuts and Seeds', now()),
+            (13,'Beef Products', now()),
+            (14,'Beverages', now()),
+            (15,'Finfish and Shellfish Products', now()),
+            (16,'Legumes and Legume Products', now()),
+            (17,'Lamb, Veal and Game', now()),
+            (18,'Baked Products', now()),
+            (19,'Sweets', now()),
+            (20,'Cereals, Grains and Pasta', now()),
+            (21,'Fast Foods', now()),
+            (22,'Mixed Dishes', now()),
+            (25,'Snacks', now()),
+            (26,'Meals', now());
+        ");
+    }
+}
