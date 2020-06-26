fc904ff447eef5791633c386c12564f0328ec35b
n.froggatt <n.froggatt@vehikl.com>
2020-06-26 16:01:24 -0400

    add foods index page

---
+++ resources/js/Pages/Foods/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <h1>made it</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            foods: Object
+        }
+    };
+</script>
