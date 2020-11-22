330f2d1401dfd73fda58acbe3220d3873708b82f
n.froggatt <n.froggatt@vehikl.com>
2020-11-21 21:31:10 -0500

    trying again

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -4,40 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Food;
 use App\Foodgroup;
-use App\Foodsource;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\DB;
 use App\Http\Resources\FoodResource;
 use Illuminate\Support\Facades\Config;
 use App\Http\Requests\CreateFoodRequest;
 use App\Http\Requests\UpdateFoodRequest;
 use App\Http\Resources\FoodgroupResource;
-use App\Http\Resources\IngredientResource;
-use phpDocumentor\Reflection\Types\Integer;
 
 class FoodController extends Controller
 {
-    // public function index(Request $request)
-    // {
-    //     $foods = Food::query()
-    //     ->userFoods()
-    //     ->sharedFoods()
-    //     ->foodgroupSearch($request->query('foodgroupSearch'))
-    //     ->descriptionSearch($request->query('descriptionSearch'))
-    //     ->aliasSearch($request->query('aliasSearch'))
-    //     ->favouritesFilter($request->query('favouritesFilter'))
-    //     ->paginate(Config::get('ml2.paginator.per_page'));
+    public function index(Request $request)
+    {
+        $foods = Food::query()
+        ->userFoods()
+        ->sharedFoods()
+        ->foodgroupSearch($request->query('foodgroupSearch'))
+        ->descriptionSearch($request->query('descriptionSearch'))
+        ->aliasSearch($request->query('aliasSearch'))
+        ->favouritesFilter($request->query('favouritesFilter'))
+        ->paginate(Config::get('ml2.paginator.per_page'));
 
-    //     $foodgroups = Foodgroup::all();
+        $foodgroups = Foodgroup::all();
 
-    //     return Inertia::render('Foods/Index', [
-    //         'page' => $foods->currentPage(),
-    //         'foods' => FoodResource::collection($foods),
-    //         'foodgroups' => FoodgroupResource::collection($foodgroups),
-    //     ]);
-    // }
+        return Inertia::render('Foods/Index', [
+            'page' => $foods->currentPage(),
+            'foods' => FoodResource::collection($foods),
+            'foodgroups' => FoodgroupResource::collection($foodgroups),
+        ]);
+    }
 
     public function create()
     {
@@ -78,7 +73,7 @@ class FoodController extends Controller
         if ($food->user_id === auth()->user()->id) {
             $food->update($request->validated());
         }
-        // return  redirect()->back();
+
         return  redirect(route('foods.index'));
 
     }
